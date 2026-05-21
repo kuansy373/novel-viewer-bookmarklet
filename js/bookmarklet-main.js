80f310ff1294f6aaacf65f2ac02caf0f907d88ad
kuansy373 <kuansy373@gmail.com>
2026-05-22 07:36:14 +0900

    test: update commit hash for mobile testling

--- js/bookmarklet-main.js
+++ js/bookmarklet-main.js
@@ -582,7 +582,7 @@
           window.makeDraggable = ${makeDraggable.toString()};
           window.parseTag = ${parseTag.toString()};
           </script>
-          <script src="https://cdn.jsdelivr.net/gh/kuansy373/novel-viewer-bookmarklet@3722395e5a7e4cbb4f5cfba72e3550f614c3dc6d/js/novel-window.js"></script>
+          <script src="https://cdn.jsdelivr.net/gh/kuansy373/novel-viewer-bookmarklet@0c00f1911790d59c5de6a72dc3fea74e5a9fb81b/js/novel-window.js"></script>
         </body>
         </html>
       `;
